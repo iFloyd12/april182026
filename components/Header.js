23cd356188a262bc461436b9fea38f1917bf2638
iFloyd12 <floyd.lagmay12@gmail.com>
2025-10-29 10:09:57 +0800

    update headers

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <nav style={{ padding: '1rem', backgroundColor: '#f8c8dc' }}>
-      <h1>Wedding of April & John</h1>
+    <nav style={{ padding: '1rem', backgroundColor: '#e9f8c8ff' }}>
+      <h1>Wedding of Ellaine & Floyd</h1>
     </nav>
   );
 }
